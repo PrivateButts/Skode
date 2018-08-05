719c86a7c865ce30ead758031894bc41e3f093f4
Randy Butts <rbutts@chq.org>
2018-08-04 23:48:25 -0400

    Added mysqlclient

--- Gallery/migrations/0003_auto_20180804_0627.py
+++ Gallery/migrations/0003_auto_20180804_0627.py
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='picture',
             name='gallery',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='Gallery.Gallery'),
+            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, to='Gallery.Gallery'),
             preserve_default=False,
         ),
     ]
