5bc46c2c75417940639277a91fd33ddab56037a8
Randy Butts <rbutts@chq.org>
2018-08-04 23:55:50 -0400

    moved staticfiles to public dir

--- Skode/settings.py
+++ Skode/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
 
 
 import raven
